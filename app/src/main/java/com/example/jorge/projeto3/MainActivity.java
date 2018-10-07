22a9feed32389fa3408fd6c74fe90092e97de2f5
Fogelman <ddfogelman@gmail.com>
2018-10-07 16:13:18 -0300

    Autenticação feita

--- app/src/main/java/com/example/jorge/projeto3/MainActivity.java
+++ app/src/main/java/com/example/jorge/projeto3/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.jorge.projeto3;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     //EditText num, msg;
     String num, msg;
     Button btnSend;
-
+    private int SEND_SMS_PERMISSION_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +43,52 @@ public class MainActivity extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendSms();
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED){
+
+                    sendSms();
+                }
+                else{
+
+                    requestSmsPermission();
+                }
+
             }
         });
+    }
+
+    private void requestSmsPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)){
+        new AlertDialog.Builder(this)
+                .setTitle("Permission neeeded")
+                .setMessage("Permissão necessária para enviar sms")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS},SEND_SMS_PERMISSION_CODE);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},SEND_SMS_PERMISSION_CODE);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == SEND_SMS_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.w("MainActivity","Permission denied");
+            }
+        }
     }
 
     private void sendSms() {
